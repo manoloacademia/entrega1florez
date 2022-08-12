a0ec6a43072fcd34a69e4f30b51c0cddda63d0e4
manoloacademia <luispablosegovia@icloud.com>
2022-08-12 10:38:25 -0300

    Se crea paquete de usuario y login/logout/register.

---
+++ wine_project/user/admin.py
@@ -0,0 +1,8 @@
+from email.mime import image
+from django.contrib import admin
+
+from .models import User_profile
+
+@admin.register(User_profile)
+class User_profile_admin(admin.ModelAdmin):
+    list_display = ['user', 'phone', 'address', 'image']
